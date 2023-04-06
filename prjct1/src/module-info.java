0ac2896f8cb69f821a7e58b88f1c3a181356f83a
oviya <oviya@LP-OVIYA>
2023-04-06 11:48:34 +0530

    First commit

---
+++ prjct1/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author oviya
+ *
+ */
+module prjct1 {
+}
---
+++ prjct1/src/prjct1/sample_prgm.java
@@ -0,0 +1,14 @@
+package prjct1;
+
+public class sample_prgm {
+
+	public static void main(String[] args) {
+		int a=4,b=5,c;
+		c=a*b;
+		System.out.println("multiple is"+c);
+
+			
+
+	}
+
+}
