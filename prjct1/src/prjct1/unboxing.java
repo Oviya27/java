4123257cea1b6ba0cdd1fc820b7bd9b440bdeabb
oviya <oviya@LP-OVIYA>
2023-04-19 14:13:15 +0530

    first msg

---
+++ prjct1/src/prjct1/unboxing.java
@@ -0,0 +1,18 @@
+package prjct1;
+
+public class unboxing {
+
+	public static void main(String[] args) {
+		Integer a=new Integer(50);
+		int n=a.intValue();
+		System.out.println(a);
+		System.out.println(n);
+		int m=a;
+		System.out.println(m);
+		Character c=new Character('A');
+		int b=c;
+		System.out.println(b);
+		
+	}
+
+}
