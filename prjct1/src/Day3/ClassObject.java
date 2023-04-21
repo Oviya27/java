bfd241226d122a18dfce85a2f174ff0710abbb2b
oviya <oviya@LP-OVIYA>
2023-04-21 13:23:06 +0530

    commit

---
+++ prjct1/src/Day3/ClassObject.java
@@ -0,0 +1,19 @@
+package Day3;
+
+public class ClassObject {
+
+	void one() {
+		System.out.println("Method one");
+	}
+	void two() {
+		System.out.println("Method two");
+	}
+	public static void main(String[] args) {
+		ClassObject obj = new ClassObject();
+		System.out.println("Method main");
+		obj.one();
+		obj.two();
+
+	}
+
+}
---
+++ prjct1/src/Day3/EncapsDemo.java
@@ -0,0 +1,13 @@
+package Day3;
+
+public class EncapsDemo {
+		public static void main(String[] args) {
+			Encaps e = new Encaps();
+			e.setId(12345);
+			e.setName("OVIYA");
+			System.out.println(e);
+
+		}
+	}
+
+
