b9d133755b654cef1a4ee3afb634fa49c100cc23
oviya <oviya@LP-OVIYA>
2023-04-21 12:01:44 +0530

    first commit

---
+++ prjct1/src/prjct1/Day7Methodoverloading.java
@@ -0,0 +1,24 @@
+package prjct1;
+
+public class Day7Methodoverloading {
+
+		public static int add(int a, int b) {
+			return a+b;
+		}
+		public static int add(int a) {
+			return a;
+		}
+		public static double add(int a, double b) {
+			return a+b;
+		}
+		public static float add(float a, int b) {
+			return a+b;
+		}
+		public static float add(float a, float b) {
+			return a+b;
+		}
+		public static String add(String a, String b) {
+			return a+b;
+	}
+
+}
