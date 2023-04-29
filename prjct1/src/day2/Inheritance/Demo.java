60f6d19bfa71a262af4f80d7d589eef116f50cf8
oviya <oviya@LP-OVIYA>
2023-04-29 13:24:43 +0530

    commit

---
+++ prjct1/src/day2/Inheritance/Demo.java
@@ -0,0 +1,11 @@
+package day2.Inheritance;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		Student stud = new Student("Oviya", "Thiruvallur", 1234567890, 21, "ABC");
+		System.out.println(stud);
+
+	}
+
+}
