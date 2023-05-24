6b5502edf8ca0e2e3b366f48743d98bba50fcf7e
oviya <oviya@LP-OVIYA>
2023-05-24 18:59:33 +0530

    first commit

---
+++ prjct1/src/Day_17/StackDemo.java
@@ -0,0 +1,26 @@
+package Day_17;
+	import java.util.*;
+	public class StackDemo {
+		public static void main(String[] args)
+	    {
+	 
+	        // Default initialization of Stack
+	        Stack stack1 = new Stack();
+	 
+	        Stack<String> stack2 = new Stack<String>();
+	 
+	        // pushing the elements
+	        stack1.push("4");
+	        stack1.push("All");
+	        stack1.push("....");
+	 
+	        stack2.push("All");
+	        stack2.push("Is");
+	        stack2.push("Well");
+	 
+	          // Printing the Stack Elements
+	        System.out.println(stack1);
+	        System.out.println(stack2);
+	    }
+	}
+
